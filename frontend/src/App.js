511a0187d627e6186d61ce46dd944acb840aca43
learning <sridurgeshv@gmail.com>
2024-11-10 13:47:42 +0530

    changes till done the process

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,14 @@
-import logo from './logo.svg';
-import { AuthProvider } from './contexts/AuthContext';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-
-/*
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
-import Welcome from './components/Welcome';
-import SignIn from './components/SignIn';
+import Welcome from './components/Welcomepg';
+import SignIn from './components/Signin';
 import Dashboard from './components/Dashboard';
-import Project from './components/Project';
-import Settings from './pages/Settings';
+// import Project from './components/Project';
+// import Settings from './pages/Settings';
 import { useAuth } from './contexts/AuthContext';
-import ProjectList from './pages/ProjectPage';
-import TeamsPage from './pages/TeamsPage';
+/* import ProjectList from './pages/ProjectPage';
+import TeamsPage from './pages/TeamsPage'; */
 
 function PrivateRoute({ children }) {
   const { user } = useAuth();
@@ -50,21 +22,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/projects" element={<ProjectList />} />
-          <Route path="/teams" element={<TeamsPage />} />
           <Route
             path="/dashboard"
             element={             
                 <Dashboard />
-            }
-          />
-          <Route
-            path="/project/:id"
-            element={
-              <PrivateRoute>
-                <Project />
-              </PrivateRoute>
             }
           />
         </Routes>
@@ -74,4 +35,3 @@ function App() {
 }
 
 export default App;
-*/
