ee4d8a21a4f886636ab8f14ebcf1d5c2921c6e49
learning <sridurgeshv@gmail.com>
2024-11-25 00:24:22 +0530

    changes to be changed

--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -56,22 +56,6 @@ const Dashboard = () => {
     setQuickLinks(updatedLinks);
   };
 
-  const getTimeAgo = (date) => {
-    const seconds = Math.floor((new Date() - new Date(date)) / 1000);
-    
-    let interval = seconds / 31536000;
-    if (interval > 1) return Math.floor(interval) + " years ago";
-    interval = seconds / 2592000;
-    if (interval > 1) return Math.floor(interval) + " months ago";
-    interval = seconds / 86400;
-    if (interval > 1) return Math.floor(interval) + " days ago";
-    interval = seconds / 3600;
-    if (interval > 1) return Math.floor(interval) + " hours ago";
-    interval = seconds / 60;
-    if (interval > 1) return Math.floor(interval) + " minutes ago";
-    return Math.floor(seconds) + " seconds ago";
-  };
-
   const fetchCollaborations = async () => {
     try {
       const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/get-collaborations`);
